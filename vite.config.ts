a253b65d535ac67a9bb65feb5321441429cee296
DanielAtDrenlia <daniel@drenlia.com>
2024-12-02 19:36:00 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: '3010',
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:3222',
+        changeOrigin: true,
+      },
+      '/attachments': {
+        target: 'http://0.0.0.0:3222',
+        changeOrigin: true,
+      },
+    },
   },
 });
